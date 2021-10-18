169cd86f4a19cd0a8c95d88c32ff8ed49efa0f2a
ruanvalente <ruan.valente@celk.net>
2021-10-18 07:52:53 -0300

    first commit

---
+++ src/main/java/com/ediaristas/EDiaristasApplication.java
@@ -0,0 +1,13 @@
+package com.ediaristas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDiaristasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiaristasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ediaristas/EDiaristasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ediaristas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDiaristasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
